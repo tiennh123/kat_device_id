a01104dbf0d1eeb58721c5da86cb9972a7a04b15
tien.nh <tien.nh@mekongpetro.com>
2021-05-19 11:16:23 +0700

    save

--- android/src/main/java/com/notrait/deviceid/DeviceIdPlugin.java
+++ android/src/main/java/com/notrait/deviceid/DeviceIdPlugin.java
@@ -15,6 +15,10 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
+import java.net.NetworkInterface;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * DeviceIdPlugin
  */
@@ -91,71 +95,26 @@ public class DeviceIdPlugin implements MethodCallHandler {
             }
             case "getMacAddress": {
                 try {
-                    int mgmtInfoBase[6];
-                    char *msgBuffer = NULL;
-                    size_t length;
-                    unsigned char macAddress[6];
-                    struct if_msghdr *interfaceMsgStruct;
-                    struct sockaddr_dl *socketStruct;
-                    NSString *errorFlag = NULL;
- 
-                    // Setup the management Information Base (mib)
-                    mgmtInfoBase[0] = CTL_NET;        // Request network subsystem
-                    mgmtInfoBase[1] = AF_ROUTE;       // Routing table info
-                    mgmtInfoBase[2] = 0;
-                    mgmtInfoBase[3] = AF_LINK;        // Request link layer information
-                    mgmtInfoBase[4] = NET_RT_IFLIST;  // Request all configured interfaces
-                
-                    // With all configured interfaces requested, get handle index
-                    if ((mgmtInfoBase[5] = if_nametoindex("en0")) == 0)
-                        errorFlag = @"if_nametoindex failure";
-                    else
-                    {
-                        // Get the size of the data available (store in len)
-                        if (sysctl(mgmtInfoBase, 6, NULL, &length, NULL, 0) < 0)
-                            errorFlag = @"sysctl mgmtInfoBase failure";
-                        else
-                        {
-                            // Alloc memory based on above call
-                            if ((msgBuffer = malloc(length)) == NULL)
-                                errorFlag = @"buffer allocation failure";
-                            else
-                            {
-                                // Get system information, store in buffer
-                                if (sysctl(mgmtInfoBase, 6, msgBuffer, &length, NULL, 0) < 0)
-                                    errorFlag = @"sysctl msgBuffer failure";
-                            }
-                        }
-                    }
-                
-                    // Befor going any further...
-                    if (errorFlag != NULL)
-                    {
-                        NSLog(@"Error: %@", errorFlag);
-                        return errorFlag;
-                    }
-                
-                    // Map msgbuffer to interface message structure
-                    interfaceMsgStruct = (struct if_msghdr *) msgBuffer;
-                
-                    // Map to link-level socket structure
-                    socketStruct = (struct sockaddr_dl *) (interfaceMsgStruct + 1);
-                
-                    // Copy link layer address data in socket structure to an array
-                    memcpy(&macAddress, socketStruct->sdl_data + socketStruct->sdl_nlen, 6);
-                
-                    // Read from char array into a string object, into traditional Mac address format
-                    NSString *macAddressString = [NSString stringWithFormat:@"%02x:%02x:%02x:%02x:%02x:%02x",
-                                                macAddress[0], macAddress[1], macAddress[2],
-                                                macAddress[3], macAddress[4], macAddress[5]];
-                    NSLog(@"Mac Address: %@", macAddressString);
-                
-                    // Release the buffer memory
-                    free(msgBuffer);
-                
-                    return macAddressString;
+                    List<NetworkInterface> all = Collections.list(NetworkInterface.getNetworkInterfaces());
+                    for (NetworkInterface nif : all) {
+                        if (!nif.getName().equalsIgnoreCase("wlan0")) continue;
 
-                } catch (Exception ex) { 
+                        byte[] macBytes = nif.getHardwareAddress();
+                        if (macBytes == null) {
+                            return "";
+                        }
+
+                        StringBuilder res1 = new StringBuilder();
+                        for (byte b : macBytes) {
+                            res1.append(Integer.toHexString(b & 0xFF) + ":");
+                        }
+
+                        if (res1.length() > 0) {
+                            res1.deleteCharAt(res1.length() - 1);
+                        }
+                        return res1.toString();
+                    }
+                } catch (Exception ex) {
                     return "02:00:00:00:00:00";
                 }
             }
